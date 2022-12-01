ab62024aea14d53f5b9e46f371202350f07fac88
holus-bolus <ustinovoleksij@gmail.com>
2022-12-01 18:38:22 +0200

    First build

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,36 @@
-import './App.css';
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
+import { Component } from "react";
+import "./App.css";
+
+const Header = () => {
+  return <h2>Hello world!</h2>;
+};
+
+class Field extends Component {
+  render() {
+    const holder = "Enter here";
+    const styledField = {
+      width: "300px",
+    };
+    return <input placeholder={holder} type="text" style={styledField} />;
+  }
+}
+
+function Button() {
+  const text = "Log in";
+  const res = () => {
+    return "Log in";
+  };
+  return <button>{res()}</button>;
+}
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header />
+      <Field />
+      <Button />
     </div>
   );
 }
+
+export { Header };
 export default App;
